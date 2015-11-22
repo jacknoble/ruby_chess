bca0198400fab2cd6e3f0bff0bfab27747823cfc
Jack Noble <jacknoble@gmail.com>
2015-11-21 20:30:13 -0800

    Add test for basic notation parser and create .gitignore

---
+++ test/test_basic_notation_parser.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../lib/basic_notation_parser'
+
+class TestBasicNotationParser < MiniTest::Unit::TestCase
+  def test_parsing
+    assert BasicNotationParser.new.parse('e2-e4') == [[6, 4], [4, 4]]
+  end
+end
